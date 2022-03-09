faaecf357ac01a25f52793030c16f0e78b41f86b
Tusharkale2 <78342663+Tusharkale2@users.noreply.github.com>
2022-03-09 17:10:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,12 @@ $password = 'Tazz@12345';
 $db_name = 'FRT';
  
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "C:\wamp64\www\Jnex\DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306 );
-echo "<script>alert('Register Sucssefully');</script>";  
+mysqli_ssl_set($conn, NULL, NULL,'/var/www/html/DigiCertGlobalRootG2.crt.pem', NULL, NULL); 
+mysqli_connect($conn, "frt.mysql.database.azure.com", "tushar@frt", 'Tazz@12345', 'frt', 3306);
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 if(isset($_POST['register'])){
 /* echo "<script>alert('Register Sucssefully');</script>";*/ 
  $name= $_POST['name']; 
