a35184bb7e281b0000e4322957b719b6867dba70
Tusharkale2 <78342663+Tusharkale2@users.noreply.github.com>
2022-03-09 16:29:07 +0530

    Update login.php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ $db_name = 'FRT';
  
 $conn = mysqli_init();
 mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootG2.crt.pem", NULL, NULL);
-mysqli_real_connect($conn,'frt.mysql.database.azure.com', 'tushar@frt','Tazz@12345', 'frt', 3306 );
+mysqli_real_connect($conn,'frt.mysql.database.azure.com', 'tushar@frt','Tazz@12345', 'frt', 3306 ,NULL, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno())
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
